87c26acfbd4a9afac96c95cc92f2b17712bc6eac
Jim Grandpre <jimtla@gmail.com>
2012-10-06 10:37:30 -0400

    Add run_local.sh

---
+++ run_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ${HUBOT_ENV-hubot} == 'jim' ]; then
+    export HUBOT_HIPCHAT_JID="27746_172794@chat.hipchat.com"
+    export HUBOT_HIPCHAT_NAME="Hubot Botson"
+    export HUBOT_HIPCHAT_PASSWORD="hubothubot"
+    export HUBOT_HIPCHAT_TOKEN="90bfca7c22fd171bb8ba2b5e2d7ebd"
+elif [ ${HUBOT_ENV-hubot} == 'ryan' ]; then
+    export HUBOT_HIPCHAT_JID="27746_172794@chat.hipchat.com"
+    export HUBOT_HIPCHAT_NAME="Hubot Botson"
+    export HUBOT_HIPCHAT_PASSWORD="hubothubot"
+    export HUBOT_HIPCHAT_TOKEN="90bfca7c22fd171bb8ba2b5e2d7ebd"
+else
+    export HUBOT_HIPCHAT_JID="27746_172794@chat.hipchat.com"
+    export HUBOT_HIPCHAT_NAME="Hubot Botson"
+    export HUBOT_HIPCHAT_PASSWORD="hubothubot"
+    export HUBOT_HIPCHAT_TOKEN="90bfca7c22fd171bb8ba2b5e2d7ebd"
+fi
+
+./bin/hubot -a hipchat -n Hubot
